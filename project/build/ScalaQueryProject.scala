807819f7374e862fc4b8d8fa45ba0e177b2d0a4d
Trond Marius Øvstetun <trond@ovstetun.no>
2011-05-28 14:13:25 +0200

    Oppdatert til scala 2.9.0 og nyeste specs.

--- project/build/ScalaQueryProject.scala
+++ project/build/ScalaQueryProject.scala
@@ -2,9 +2,9 @@ import sbt._
 
 class ScalaQueryProject(info: ProjectInfo) extends DefaultProject(info) {
 	
-  val scalaQuery = "org.scalaquery" %% "scalaquery" % "0.9.2" withSources()
+  val scalaQuery = "org.scalaquery" %% "scalaquery" % "0.9.4" withSources()
 
-  val specs2 = "org.specs2" %% "specs2" % "1.2" % "test" withSources()
+  val specs2 = "org.specs2" %% "specs2" % "1.3" % "test" withSources()
   val h2 = "com.h2database" % "h2" % "1.3.153" % "test"
   
   val snapshots = ScalaToolsSnapshots
