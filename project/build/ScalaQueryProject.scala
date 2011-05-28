c179f19526bb1cfcafb7ef8728509d1ed5bdd4d0
Trond Marius Øvstetun <trond@ovstetun.no>
2011-05-28 13:15:55 +0200

    Oppdatert versjoner av scalaquery og specs2

--- project/build/ScalaQueryProject.scala
+++ project/build/ScalaQueryProject.scala
@@ -2,9 +2,9 @@ import sbt._
 
 class ScalaQueryProject(info: ProjectInfo) extends DefaultProject(info) {
 	
-  val squeryl = "org.squeryl" % "squeryl_2.8.1" % "0.9.4-RC6" withSources()
+  val scalaQuery = "org.scalaquery" %% "scalaquery" % "0.9.2" withSources()
 
-  val specs2 = "org.specs2" %% "specs2" % "1.1" % "test" withSources()
+  val specs2 = "org.specs2" %% "specs2" % "1.2" % "test" withSources()
   val h2 = "com.h2database" % "h2" % "1.3.153" % "test"
   
   val snapshots = ScalaToolsSnapshots
