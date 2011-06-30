3d5913cb59cf449c100029c2de7ec4b1c61e90a9
Trond Marius Øvstetun <trond@ovstetun.no>
2011-06-30 14:53:26 +0200

    Avhengighet til Liquibase og skript for oppretting av User-tabell i database.

---
+++ db.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+java -jar $HOME/.ivy2/cache/org.liquibase/liquibase-core/jars/liquibase-core-2.0.1.jar \
+  --driver=org.h2.Driver \
+  --classpath=target/scala-2.9.0.1/classes:$HOME/.ivy2/cache/com.h2database/h2/jars/h2-1.3.153.jar \
+  --changeLogFile=no/ovstetun/db/changelog-1.0.xml \
+  --url=jdbc:h2:test \
+  --username=sa \
+  --password= \
+  update
