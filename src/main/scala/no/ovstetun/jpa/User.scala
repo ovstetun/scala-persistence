ab11be16de235737dfb2e885fdf6b6968d2196d8
Trond Marius Øvstetun <trond@ovstetun.no>
2011-08-26 13:51:57 +0200

    Switch to Eclipselink and update to User.

--- src/main/scala/no/ovstetun/jpa/User.scala
+++ src/main/scala/no/ovstetun/jpa/User.scala
@@ -1,12 +1,14 @@
 package no.ovstetun.jpa
 
-import javax.persistence.{GenerationType, GeneratedValue, Id, Entity}
+import javax.persistence._
 
 @Entity
+@Table(name = "users")
 class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   var id : Long = _
   var firstname : String = _
   var lastname : String = _
+  var email : String = _
 }
