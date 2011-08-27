cfbd8f1c16b0b4b2507158a6e3794b32431614b4
Trond Marius Øvstetun <trond@ovstetun.no>
2011-08-27 15:00:21 +0200

    Add explicit IDs to all genres.

--- src/main/scala/no/ovstetun/Genre.scala
+++ src/main/scala/no/ovstetun/Genre.scala
@@ -3,12 +3,12 @@ package no.ovstetun
 object Genre extends Enumeration {
   type Genre = Value
 
-  val Rock = Value
-  val Pop = Value
-  val Classic = Value
-  val Blues = Value
-  val Rap = Value
-  val HipHop = Value
-  val Alternative = Value
+  val Rock = Value(1)
+  val Pop = Value(2)
+  val Classic = Value(3)
+  val Blues = Value(4)
+  val Rap = Value(5)
+  val HipHop = Value(6)
+  val Alternative = Value(7)
 
 }
