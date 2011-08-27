838912ce312470ebe21f68ce77bcb157d3b51478
Trond Marius Øvstetun <trond@ovstetun.no>
2011-08-27 11:03:52 +0200

    added Genre class for Music example

---
+++ src/main/scala/no/ovstetun/Genre.scala
@@ -0,0 +1,14 @@
+package no.ovstetun
+
+object Genre extends Enumeration {
+  type Genre = Value
+
+  val Rock = Value
+  val Pop = Value
+  val Classic = Value
+  val Blues = Value
+  val Rap = Value
+  val HipHop = Value
+  val Alternative = Value
+  
+}
