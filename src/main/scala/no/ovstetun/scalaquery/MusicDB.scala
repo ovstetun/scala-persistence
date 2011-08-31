92b27d81328db6e56488ca866eb175635b16f234
Trond Marius Øvstetun <trond@ovstetun.no>
2011-08-31 14:07:25 +0200

    Defined schema for the music database using scalaquery.

---
+++ src/main/scala/no/ovstetun/scalaquery/MusicDB.scala
@@ -0,0 +1,60 @@
+package no.ovstetun.scalaquery
+
+import org.scalaquery.ql._
+import extended.{ExtendedProfile, ExtendedTable => Table}
+import java.sql.Date
+
+trait MusicDB {
+  self : ExtendedProfile =>
+  import self.Implicit._
+
+  object Artists extends Table[(Int, String, String, Date, Option[Date])]("ARTISTS") {
+    def id = column[Int]("ID", O PrimaryKey, O AutoInc)
+    def name = column[String]("NAME")
+    def biography = column[String]("BIOGRAPHY")
+    def founded = column[Date]("FOUNDED")
+    def split = column[Option[Date]]("SPLIT")
+
+    def * = id ~ name ~ biography ~ founded ~ split
+  }
+  object Albums extends Table[(Int, String, Date, Option[Int], Int)]("ALBUMS") {
+    def id = column[Int]("ID", O PrimaryKey, O AutoInc)
+    def name = column[String]("NAME", O NotNull)
+    def release = column[Date]("RELEASE")
+    def rating = column[Option[Int]]("RATING")
+    def artist_id = column[Int]("ARTIST_ID")
+
+    def artist = foreignKey("albums_artists_fk", artist_id, Artists)(_.id)
+
+    def * = id ~ name ~ release ~ rating ~ artist_id
+  }
+  object Songs extends Table[(Int, String, Int, Int, Int)]("SONGS") {
+    def id = column[Int]("ID", O PrimaryKey, O AutoInc)
+    def name = column[String]("NAME", O NotNull)
+    def duration = column[Int]("DURATION")
+    def tracknumber = column[Int]("TRACKNUMBER")
+    def album_id = column[Int]("ALBUM_ID")
+
+    def album = foreignKey("songs_albums_fk", album_id, Albums)(_.id)
+
+    def * = id ~ name ~ duration ~ tracknumber ~ album_id
+  }
+  object Persons extends Table[(Int, String, String, Option[String])]("PERSONS") {
+    def id = column[Int]("ID", O PrimaryKey, O AutoInc)
+    def firstname = column[String]("FIRSTNAME", O NotNull)
+    def lastname = column[String]("LASTNAME", O NotNull)
+    def biography = column[Option[String]]("BIOGRAPHY", O NotNull)
+
+    def * = id ~ firstname ~ lastname ~ biography
+  }
+  object PersonsArtists extends Table[(Int, Int)]("PERSONS_ARTISTS") {
+    def artist_id = column[Int]("ARTIST_ID", O NotNull)
+    def person_id = column[Int]("PERSON_ID", O NotNull)
+
+    def pk = primaryKey("personartists_pk", artist_id ~ person_id)
+    def artist = foreignKey("person_artist_a_fk", artist_id, Artists)(_.id)
+    def person = foreignKey("person_artist_p_fk", person_id, Persons)(_.id)
+
+    def * = artist_id ~ person_id
+  }
+}
---
+++ src/test/scala/no/ovstetun/scalaquery/MusicScalaQuerySpec.scala
@@ -0,0 +1,9 @@
+package no.ovstetun
+package scalaquery
+
+import org.specs2.mutable.Specification
+import no.ovstetun.DBSupport
+
+class MusicScalaQuerySpec extends Specification with DBSupport {
+  "scalaquery" in pending
+}
